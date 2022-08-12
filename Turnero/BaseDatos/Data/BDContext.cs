01c43c19c7f50286671cf185cd37a0d30c793999
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-08-12 17:24:48 -0300

    Data reubicada

---
+++ Turnero/BaseDatos/Data/BDContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Turnero.BaseDatos.Data.Entidades;
+
+namespace Turnero.BaseDatos.Data
+{
+    public class BDContext : DbContext
+    {
+        public BDContext(
+            DbContextOptions<BDContext> options
+        ) : base(options)
+        {
+        }
+
+
+        public DbSet<Turno> TablaTurnos { get; set; }
+        public DbSet<Cliente> TablaClientes { get; set; }
+        public DbSet<Peluquero> TablaPeluqueros { get; set; }
+
+
+
+    }
+}

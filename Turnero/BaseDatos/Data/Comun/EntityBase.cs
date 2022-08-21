61ad75389bfc5e86eaa9e0d250d4605fb9a1cc87
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-08-21 20:59:46 -0300

    Definicion de Clave Primaria

--- Turnero/BaseDatos/Data/Comun/EntityBase.cs
+++ Turnero/BaseDatos/Data/Comun/EntityBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace Turnero.BaseDatos.Data.Comun
 {
     public class EntityBase
     {
+        [Key] /// Definimos este Id como la Clave primaria.
         public int Id { get; set; }
-
     }
 }

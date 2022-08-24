df40c944ba361b1bfe2c04d145769c6bcf2d9aba
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-08-23 23:40:16 -0300

    PeluqueroDTO

---
+++ Turnero/Shared/DTO_Back/Peluquero/PeluqueroDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turnero.Shared.DTO_Back.Peluquero
+{
+    public class PeluqueroDto
+    {
+        [Required(ErrorMessage = "Campo requerido")]
+        [MinLength(2, ErrorMessage = "Error de longuitud, minimo de {1} caracteres")]
+        [MaxLength(50, ErrorMessage = "Error de longuitud, maximo de {1} caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        public string ImagenPerfil { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [MinLength(2, ErrorMessage = "Error de longuitud, minimo de {1} caracteres")]
+        [MaxLength(50, ErrorMessage = "Error de longuitud, maximo de {1} caracteres")]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [MinLength(8, ErrorMessage = "Error de longuitud, minimo de {1} caracteres")]
+        [MaxLength(10, ErrorMessage = "Error de longuitud, maximo de {1} caracteres")]
+        public string DNI { get; set; }
+    }
+}

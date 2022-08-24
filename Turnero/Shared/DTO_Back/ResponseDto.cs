c0dba946001230e3e0cab4762de5c57dbf4b6358
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-08-23 23:47:41 -0300

    DTO Back

---
+++ Turnero/Shared/DTO_Back/ResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Turnero.Shared.Comun;
+
+namespace Turnero.Shared.DTO_Back
+{
+    public class ResponseDto<TypeData>
+    {
+        public TypeData Data { get; set; }
+        public List<ModelErrors> ErrorModels { get; set; }
+        public string MessageError { get; set; }
+    }
+}

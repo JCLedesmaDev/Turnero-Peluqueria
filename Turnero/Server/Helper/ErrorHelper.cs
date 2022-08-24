903431bd930bef49e8a2b4ec6bf93c658e2785c1
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-08-23 23:40:49 -0300

    Helper para obtener error ModelState

---
+++ Turnero/Server/Helper/ErrorHelper.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Turnero.Shared.Comun;
+
+namespace Turnero.Helper
+{
+    public class ErrorHelper
+    {
+
+        public static List<ModelErrors> GetModelStateErrors(ModelStateDictionary Model)
+        {
+            return Model.Select(x=> new ModelErrors(){ 
+                Key = x.Key, 
+                Messages = x.Value.Errors.Select(y=>y.ErrorMessage).ToList()
+
+            }).ToList();
+        }
+    }
+
+}

17daf694fb2693acadef15c1117431024d9c60c1
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-08-23 23:47:50 -0300

    Comun

---
+++ Turnero/Shared/Comun/ModelErrors.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turnero.Shared.Comun
+{
+    public class ModelErrors
+    {
+        public string Key { get; set; }
+        public List<string> Messages { get; set; }
+    }
+}

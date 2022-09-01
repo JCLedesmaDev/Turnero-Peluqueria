c55f572e3e041a8ea9ba4752b1268de1b182567d
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-09-01 14:56:16 -0300

    class static MargenTurno

---
+++ Turnero/Shared/Comun/MargenTurno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turnero.Shared.Comun
+{
+    public class MargenTurno
+    {
+        public static double SumarExtraTiempo = 30;
+        public static double RestarExtraTiempo = -15;
+    }
+}

68f747ee5918231756abfa6b9245fd0d7c6fc7e8
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2022-09-16 09:26:23 -0300

    .

--- Turnero/Client/Program.cs
+++ Turnero/Client/Program.cs
@@ -1,5 +1,3 @@
-global using Turnero.Client.Models;
-
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Turnero.Client;
@@ -11,3 +9,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
